bf5919f08c2a5df188f73c1b1e94b230a040cb1d
Gerardo Considerato <Gary92@users.noreply.github.com>
2020-02-18 00:11:30 +0100

    Second commit

---
+++ build/precache-manifest.9617e548233f1edf6b9357179a3531e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52466adb12c84aab2acb7d46f743930",
+    "url": "/react-15-puzzle/index.html"
+  },
+  {
+    "revision": "ef9835e9fd017b4580db",
+    "url": "/react-15-puzzle/static/css/main.ba0ef4b0.chunk.css"
+  },
+  {
+    "revision": "37337d2328f925547c2b",
+    "url": "/react-15-puzzle/static/js/2.da0b09f5.chunk.js"
+  },
+  {
+    "revision": "ef9835e9fd017b4580db",
+    "url": "/react-15-puzzle/static/js/main.83169faf.chunk.js"
+  },
+  {
+    "revision": "c888cbb19fd460e61437",
+    "url": "/react-15-puzzle/static/js/runtime~main.8e20e100.js"
+  }
+]);
