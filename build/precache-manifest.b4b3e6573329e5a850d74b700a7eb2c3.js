0ecbc84f602d0cf9dbdd23130314237b81437929
Gerardo Considerato <Gary92@users.noreply.github.com>
2020-02-20 02:00:05 +0100

    Added PWA logic

--- build/precache-manifest.b4b3e6573329e5a850d74b700a7eb2c3.js
+++ build/precache-manifest.b4b3e6573329e5a850d74b700a7eb2c3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6053adb6a7c22449f90c17fec9538f3e",
+    "revision": "05b3f0bc6440c0276cd715890581c8fa",
     "url": "/react-15-puzzle/index.html"
   },
   {
